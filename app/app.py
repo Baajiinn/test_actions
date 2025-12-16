500f738d4afcce2c17dda555d34707b9e726f42c
Baajiinn <craftesdras@gmail.com>
2025-12-16 16:06:10 +0100

    Fix : app.py

--- app/app.py
+++ app/app.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World</h1>"
+def hello():
+    return "Hello World"
+
+@app.route("/ping")
+def ping():
+    return "ping"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
